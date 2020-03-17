87029f3a414af96e9bd5dac2ac151b9ca8b3254d
Abhinav Dubey <49772834+abhinavdubeyad9@users.noreply.github.com>
2020-03-17 19:22:44 +0530

    Updates

---
+++ precache-manifest.06be353a74abd9ffa7299188d860fa37.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a65684a6207ea1d22304afd1ccab3f",
+    "url": "/movie-database/index.html"
+  },
+  {
+    "revision": "ffccec7bb70700278de2",
+    "url": "/movie-database/static/css/main.2ebf4197.chunk.css"
+  },
+  {
+    "revision": "a358f27de60e7d339c05",
+    "url": "/movie-database/static/js/2.2e0c105e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie-database/static/js/2.2e0c105e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffccec7bb70700278de2",
+    "url": "/movie-database/static/js/main.ab6db73d.chunk.js"
+  },
+  {
+    "revision": "2708f97a9c9cd8e6038a",
+    "url": "/movie-database/static/js/runtime-main.8f3ba8e1.js"
+  },
+  {
+    "revision": "fe2b12b0a27aaf0d2ef5bb14451308c2",
+    "url": "/movie-database/static/media/HARRP___.fe2b12b0.woff"
+  },
+  {
+    "revision": "19845b66db3f1c5940d002e356685b85",
+    "url": "/movie-database/static/media/bg.19845b66.jpg"
+  }
+]);
